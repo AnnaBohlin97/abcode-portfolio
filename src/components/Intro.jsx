01ea21a23765152adf199565e8b5983851531d6c
annaBohlin <anna@bohlinoffice.se>
2023-05-11 22:42:49 +0200

    updated img paths

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,9 @@
+import Me from "../assets/me.jpeg"
+
 export default function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-				<img src="src/assets/me.jpeg" alt="Picture of me" className="w-56 rounded-full"/>
+				<img src={Me} alt="Picture of me" className="w-56 rounded-full"/>
         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Anna</h1>
 				<p className="text-base md:text-xl mb-3 font-medium">Fullstack Web Developer</p>
 				
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,4 +1,6 @@
 // import {SiReact, SiJavascript, SiTailwindcss, SiNodedotjs, SiExpress, SiSequelize, SiPostgresql, SiGit, SiGithub, SiCss3} from "react-icons/si"
+import FinDash from "../assets/findash.png"
+import Handledning from "../assets/handledning.png"
 
 export default [
 	{
@@ -10,14 +12,14 @@ export default [
 	},
 	{
 		title: "Handledning.se",
-		imgUrl: "src/assets/handledning.png",
+		imgUrl: {Handledning},
 		stack: ["React.js", "CSS"],
 		codeLink: "https://github.com/ave-se/react-app",
 		liveLink: "https://ubiquitous-gaufre-0e6db5.netlify.app/about"
 	},
 	{
 		title: "Financial Dashboard",
-		imgUrl: "src/assets/findash.png",
+		imgUrl: {FinDash},
 		stack: ["React.js", "CSS"],
 		codeLink: "https://github.com/AnnaBohlin97/financial-dashboard",
 		liveLink: "https://cheery-brigadeiros-8234f7.netlify.app/"
