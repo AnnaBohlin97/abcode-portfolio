6beaf521a573f80aca37577cde462f13ecc53c2b
annaBohlin <anna@bohlinoffice.se>
2023-05-11 23:00:10 +0200

    padding

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,6 @@ export default function Intro() {
 				<img src={Me} alt="Picture of me" className="w-56 rounded-full drop-shadow-lg"/>
         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Anna</h1>
 				<p className="text-base md:text-xl mb-3 font-medium">Fullstack Web Developer</p>
-				
         <p className="text-sm max-w-xl mb-6 font-bold">
 					Hello! As you might have guessed, my name is Anna. Welcome to my portfolio website!
 					I'm a 25 year old developer from Uppsala, and currently I'm in Stockholm studying at the Frontend Developer program at Hyper Island.
