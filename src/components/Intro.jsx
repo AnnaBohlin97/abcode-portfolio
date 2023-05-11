6511d7936b16f9bea5eac1074382ac255680fde4
annaBohlin <anna@bohlinoffice.se>
2023-05-11 22:56:23 +0200

    dropshadow

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import Me from "../assets/me.jpeg"
 export default function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-24 pb-24 ">
-				<img src={Me} alt="Picture of me" className="w-56 rounded-full"/>
+				<img src={Me} alt="Picture of me" className="w-56 rounded-full drop-shadow-lg"/>
         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Anna</h1>
 				<p className="text-base md:text-xl mb-3 font-medium">Fullstack Web Developer</p>
 				
