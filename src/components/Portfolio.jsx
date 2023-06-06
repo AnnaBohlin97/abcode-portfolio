f4a4fd8942a627f522809bc049af5ced30804da2
annaBohlin <anna@bohlinoffice.se>
2023-06-06 23:19:30 +0200

    added descriptions and styling to Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,12 @@
 import portfolio from "../data/portfolio"
 import PortfolioItem from "./PortfolioItem"
+import { Link } from "react-scroll";
+import Me from "../assets/me.jpeg"
+import Chevron from "../assets/chevron.svg"
 
 export default function Portfolio() {
 	return (
-		<div name="Projects" className="flex flex-col pt-20 pb-32 my-48 items-center justify-center">
+		<div name="Projects" className="flex flex-col pt-8 pb-32 my-48 mx-4 md:mx-16 items-center justify-center">
 			<h2 className="text-3xl md:text-5xl dark:text-white mb-1 md:mb-6 font-bold">Projects</h2>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 				{portfolio.map((project, index) => (
@@ -14,9 +17,15 @@ export default function Portfolio() {
 						stack={project.stack}
 						codeLink={project.codeLink}
 						liveLink={project.liveLink}
+						description={project.description}
 					/>
 				))}
 			</div>
+			<button className="animate-bounce w-12 mt-8 mb-16">
+          <Link to="Skills" smooth={true} duration={500} offset={-80}>
+            <img src={Chevron} alt="" />
+          </Link>
+        </button>
 		</div>
 	)
 }
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,20 +1,28 @@
-export default function PortfolioItem({title, imgUrl, stack, codeLink, liveLink}) {
-	return (
-		<div className="transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 shadow-md hover:shadow-xl duration-300 border-2 border-stone-900 rounded-md overflow-hidden">
-			<a href={codeLink}>
-				<img src={imgUrl} alt="" className="w-full h-36 md:h-48 object-cover cursor-pointer"/>
-			</a>
-			<div className="w-full p-4">
-				<h3 className="text-lg md:text-xl mb-2 md:mb.3 font-semibold">{title}</h3>
-				<p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
-					{stack.map((item, index) => (
-						<span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md" key={index}>
-							{item}
-						</span>
-					))}
-				</p>
-
-			</div>
-		</div>
-	)
+export default function PortfolioItem({ title, imgUrl, stack, codeLink, liveLink, description }) {
+  return (
+    <div className="transition ease-in-out delay-150 hover:translate-y-1 hover:scale-105 shadow-md hover:shadow-xl duration-300 border-2 border-stone-900 rounded-md overflow-hidden">
+      <img src={imgUrl} alt="" className="w-full h-36 md:h-48 object-cover" />
+      <div className="w-full p-4">
+        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm mb-2 md:mb-3">
+          {stack.map((item, index) => (
+            <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md" key={index}>
+              {item}
+            </span>
+          ))}
+        </p>
+        <p className="text-xs md:text-sm self-center">{description}</p>
+        <div className="flex justify-center mt-4 gap-2">
+          <a href={codeLink} className="px-2 py-1 font-semibold border-2 border-stone-900 rounded-md transition ease-in-out delay-150 duration-300 hover:bg-teal-800 hover:text-white shadow-sm">
+            <button className="">Code</button>
+          </a>
+          {liveLink && (
+            <a href={liveLink} className="px-2 py-1 font-semibold border-2 border-stone-900 rounded-md transition ease-in-out delay-150 duration-300 hover:bg-teal-800 hover:text-white">
+              <button className="">Live</button>
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
